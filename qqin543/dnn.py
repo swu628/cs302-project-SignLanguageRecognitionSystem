f4ae9cb3c26514f4bebcdb739d97f70ccaa5c841
kylyn <swu628@aucklanduni.ac.nz>
2023-04-14 16:41:52 +1200

    added dnn model and resolve bug

--- qqin543/dnn.py
+++ qqin543/dnn.py
@@ -2,16 +2,37 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Logistic Regression
-class logisticRegressionModel(nn.Module):
 
-    def __init__(self, in_channels, num_classes):
+class DNNModel(nn.Module):
+    """Feedfoward neural network with 2 hidden layer"""
+    def __init__(self, in_size, out_size):
         super().__init__()
-        self.linear = nn.Linear(in_channels*28*28, num_classes)
+        # hidden layer 1
+        self.linear1 = nn.Linear(in_size, 512)
+        # hidden layer 2
+        self.linear2 = nn.Linear(512, 256)
+        # hidden layer 3
+        self.linear3 = nn.Linear(256, 128)
+        # output layer  
+        self.linear4 = nn.Linear(128, out_size)
         
     def forward(self, xb):
-        xb = xb.reshape(-1, 1*28*28)
-        out = self.linear(xb)
+        # Flatten the image tensors
+        out = xb.view(xb.size(0), -1)
+        # Get intermediate outputs using hidden layer 1
+        out = self.linear1(out)
+        # Apply activation function
+        out = F.relu(out)
+        # Get intermediate outputs using hidden layer 2
+        out = self.linear2(out)
+        # Apply activation function
+        out = F.relu(out)
+        # Get inermediate outputs using hidden layer 3
+        out = self.linear3(out)
+        # Apply a activation function
+        out = F.relu(out)
+        # Get predictions using output layer
+        out = self.linear4(out)
         return out
     
     def training_step(self, batch):
@@ -25,7 +46,7 @@ class logisticRegressionModel(nn.Module):
         out = self(images)                    # Generate predictions
         loss = F.cross_entropy(out, labels)   # Calculate loss
         acc = accuracy(out, labels)           # Calculate accuracy
-        return {'val_loss': loss.detach(), 'val_acc': acc.detach()}
+        return {'val_loss': loss, 'val_acc': acc}
         
     def validation_epoch_end(self, outputs):
         batch_losses = [x['val_loss'] for x in outputs]
@@ -36,7 +57,7 @@ class logisticRegressionModel(nn.Module):
     
     def epoch_end(self, epoch, result):
         print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
-    
+
 def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
     return torch.tensor(torch.sum(preds == labels).item() / len(preds))
--- qqin543/train.py
+++ qqin543/train.py
@@ -1,11 +1,8 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-import cv2
-import torch.nn.functional as F
-from cnn import CNNModel
 from loadDataset import loadData
+from cnn import CNNModel
 from logisticRegression import logisticRegressionModel
+from dnn import DNNModel
 
 class trainModel:
 
@@ -18,7 +15,7 @@ class trainModel:
         # If the user choose lenet5 model, it will call the leNet5 class
         if self.selectModelComboBox.currentIndex() == 1:
             
-            model = logisticRegressionModel(in_channels=1, num_classes=26)
+            model = logisticRegressionModel(in_channels = 1, num_classes = 26)
 
             def evaluate(model, val_loader):
                 outputs = [model.validation_step(batch) for batch in val_loader]
@@ -45,7 +42,7 @@ class trainModel:
         elif self.selectModelComboBox.currentIndex() == 2:
 
             # import cnn model
-            model = CNNModel(in_channels=1, num_classes=26)
+            model = CNNModel(in_channels = 1, num_classes = 26)
 
             # train the model
             @torch.no_grad()
@@ -77,6 +74,30 @@ class trainModel:
             history = fit(epochNum, 0.001, model, train_dataloder, validation_dataloader)
        
         elif self.selectModelComboBox.currentIndex() == 3:
-            print("VGG16")
+
+            model = DNNModel(784, out_size = 26)
+            history = []
+
+            def evaluate(model, val_loader):
+                outputs = [model.validation_step(batch) for batch in val_loader]
+                return model.validation_epoch_end(outputs)
+
+            def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+                optimizer = opt_func(model.parameters(), lr)
+                for epoch in range(epochs):
+                    # Training Phase 
+                    for batch in train_loader:
+                        loss = model.training_step(batch)
+                        loss.backward()
+                        optimizer.step()
+                        optimizer.zero_grad()
+                    # Validation phase
+                    result = evaluate(model, val_loader)
+                    model.epoch_end(epoch, result)
+                    history.append(result)
+                return history
+
+            history += fit(epochNum, .001, model, train_dataloder, validation_dataloader)
+
         else:
             print("Please select a model")
